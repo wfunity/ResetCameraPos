6c3a9bc90f83f51d2b7f3ef0cb9ed141fec6904a
MarkGT <wfunity@163.com>
2017-09-14 14:37:31 +0800

    Add files via upload

---
+++ ResetCameraPos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+public class ResetCameraPos : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+      //  VRSettings.enabled = true;
+        Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+            Valve.VR.OpenVR.Compositor.SetTrackingSpace(
+  Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+        }
+    }
+}
